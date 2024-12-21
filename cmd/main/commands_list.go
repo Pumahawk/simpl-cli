4015ca283f6da81797233af71d464d550f5f08b2
Lorenzo Gandino <lorenzoga94@gmail.com>
2024-12-21 14:24:03 +0100

    Refactor commands list

--- cmd/main/commands_list.go
+++ cmd/main/commands_list.go
@@ -3,10 +3,5 @@ package main
 import "github.com/pumahawk/simplcli/lib/command/login"
 
 var DefinedCommands = []SubCommand{
-	LoginCommand,
-}
-
-var LoginCommand = SubCommand{
-	Name: "login",
-	Exec: login.Exec,
+	{"login", login.Exec},
 }
